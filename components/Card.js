cfb9b7c4ef509e81b5338b8055d73d30731cb084
NUR UZ ZAMAN <mail.nuruzzaman@gmail.com>
2022-10-24 22:21:02 +0600

    added card component

--- components/Card.js
+++ components/Card.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 export const Card = () => {
   return (
-<div className="card w-96 bg-primary text-primary-content">
+<div className="card w-80 text-primary-content m-7 bg-gray-800 ">
   <div className="card-body">
     <h2 className="card-title">Card title!</h2>
     <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
+    <div className="card-actions justify-center">
       <button className="btn">Buy Now</button>
     </div>
   </div>
