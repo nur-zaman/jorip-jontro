b3efc37aa6db376aba01a3ee77af856aa7222eb9
NUR UZ ZAMAN <mail.nuruzzaman@gmail.com>
2022-10-24 22:27:16 +0600

    fixed notification icon

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,4 @@
-import Avatar, { genConfig } from 'react-nice-avatar'
-import { useEffect, useState } from 'react'
+import Avatar from 'react-nice-avatar'
 import Link from 'next/link';
 
 function Navbar() {
@@ -21,8 +20,6 @@ function Navbar() {
         "shirtColor": "#9287FF",
         "bgColor": "#F4D150"
     }
-   
-    // useEffect(() => setConfig(genConfig()), [])
     return (
         <>
         <div class="navbar bg-base-100">
@@ -33,14 +30,14 @@ function Navbar() {
     <div class="flex-1">
       <a class="btn btn-ghost normal-case text-xl">Jorip Jontro</a>
     </div>
+
     <div class="flex-none gap-2">
-      {/* <div class="form-control">
-        <input type="text" placeholder="Search" class="input input-bordered" />
-      </div> */}
+    <button className="btn btn-ghost btn-circle">
             <div className="indicator">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
         <span className="badge badge-xs badge-primary indicator-item"></span>
       </div>
+      </button>
       <div class="dropdown dropdown-end">
         <label tabindex="0" class="btn btn-ghost btn-circle avatar">
           <div class="w-10 rounded-full">
