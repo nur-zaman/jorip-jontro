0a8846f6d02d34aea12ebf62af626365b412d8cb
NUR UZ ZAMAN <mail.nuruzzaman@gmail.com>
2022-11-04 16:53:08 +0600

    added auth 🔐

--- components/Login.js
+++ components/Login.js
@@ -1,36 +1,63 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 export default function Login() {
   return (
     <div className="hero min-h-screen bg-base-200">
-    <div className="hero-content flex-col lg:flex-row-reverse">
-      <div className="text-center lg:text-left">
-        <h1 className="text-5xl font-bold">Login now!</h1>
-        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      </div>
-      <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-        <div className="card-body">
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
-            <input type="text" placeholder="email" className="input input-bordered" />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Password</span>
-            </label>
-            <input type="text" placeholder="password" className="input input-bordered" />
-            <label className="label">
-              <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-            </label>
-          </div>
-          <div className="form-control mt-6">
-            <button className="btn btn-primary">Login</button>
-          </div>
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Really POG Description about the project that Ill remove soon
+          </p>
+        </div>
+        <div className="card w-full max-w-sm flex-shrink-0 bg-base-100 shadow-2xl">
+          <form action="/api/login" method="post">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="email"
+                  className="input-bordered input"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="text"
+                  name="password"
+                  placeholder="password"
+                  className="input-bordered input"
+                />
+                <label className="label">
+                  <a href="#" className="link-hover label-text-alt link">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  type="submit"
+                  value="Login"
+                  className="btn-primary btn"
+                ></input>
+                {/* <input type="submit" className="btn-primary btn">
+                  Login
+                </input> */}
+                <Link href={"signup"}>
+                  <button className="btn-primary btn mt-2">Sign Up</button>
+                </Link>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
---
+++ pages/signup.js
@@ -0,0 +1,7 @@
+import Register from "../components/Register";
+
+export function signup() {
+  return <Register></Register>;
+}
+
+export default signup;
